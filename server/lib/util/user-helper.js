73f4d62163a83502c27f4e90147d4cc2328fd406
Lokasenna <jalovatt@hotmail.com>
2018-11-09 09:53:40 -0700

    More styling

--- server/lib/util/user-helper.js
+++ server/lib/util/user-helper.js
@@ -3,21 +3,27 @@
 const Chance = require("chance");
 const chance = new Chance();
 
+const pirateNameGenerator = require("pirate-name-generator");
+
 const md5 = require('md5');
 
 module.exports = {
 
   generateRandomUser: () => {
     const gender    = chance.gender();
-    const firstName = chance.first({gender: gender});
-    const lastName  = chance.last();
+    // const firstName = chance.first({gender: gender});
+    // const lastName  = chance.last();
+    const generatedName = pirateNameGenerator().match(/(\w+) '[^']+' (\w+)/);
+    console.log(generatedName);
+    const firstName = generatedName[1];
+    const lastName = generatedName[2];
     const userName  = firstName + " " + lastName;
 
     let userHandle = "@";
     if (Math.random() > 0.5) {
       let prefix    = chance.prefix({gender: gender});
       prefix = prefix.replace(".", "");
-      userHandle += prefix
+      userHandle += prefix;
     }
 
     userHandle += lastName;
