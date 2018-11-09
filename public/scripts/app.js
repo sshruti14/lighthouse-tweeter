c12001fffe21c195b2e56fce174fd4cc2da2681b
Lokasenna <jalovatt@hotmail.com>
2018-11-09 16:59:15 -0700

    Merge a stray script into the main file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -148,6 +148,19 @@ function attachListeners() {
 
   });
 
+  $(".new-tweet textarea").on("input", null, null, function (event) {
+
+    const charsLeft = globals.MAX_TWEET_LENGTH - this.value.length;
+    const counter = $(this).siblings(".counter")[0];
+
+    counter.innerText = charsLeft;
+
+    (charsLeft < 0)
+      ? counter.classList.add("counter-invalid")
+      : counter.classList.remove("counter-invalid");
+
+  });
+
 }
 
 
